9a317ae7e7575a105ccca85a81f8ec77b521df21
sgray <s.gray@circle-eight.com>
2024-01-13 12:40:33 +1300

    Move player down, stop at bottom of canvas

--- index.js
+++ index.js
@@ -4,17 +4,24 @@ const c = canvas.getContext('2d');
 canvas.width = 1024;
 canvas.height = 576;
 
-c.fillStyle = 'white';
-c.fillRect(0, 0, canvas.width, canvas.height);
+// c.fillStyle = 'white';
+// c.fillRect(0, 0, canvas.width, canvas.height);
 
 let y = 100;
+const height = 100;
+let bottom = y + 100;
 
 function animate() {
 	window.requestAnimationFrame(animate);
 	c.clearRect(0, 0, canvas.width, canvas.height);
+
 	c.fillStyle = 'red';
-	c.fillRect(100, y, 100, 100);
-	y++;
+	c.fillRect(100, y, 100, height);
+
+	if (bottom <canvas.height) {
+		y++;	
+		bottom = y + 100;
+	}	
 }
 
 animate();
