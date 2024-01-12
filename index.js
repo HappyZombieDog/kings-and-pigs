74f2229d4b35f06e544bc942e1ef1e2565a2ef6c
sgray <s.gray@circle-eight.com>
2024-01-13 12:27:53 +1300

    Move player down past bottom of canvas

---
+++ index.js
@@ -0,0 +1,20 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+canvas.width = 1024;
+canvas.height = 576;
+
+c.fillStyle = 'white';
+c.fillRect(0, 0, canvas.width, canvas.height);
+
+let y = 100;
+
+function animate() {
+	window.requestAnimationFrame(animate);
+	c.clearRect(0, 0, canvas.width, canvas.height);
+	c.fillStyle = 'red';
+	c.fillRect(100, y, 100, 100);
+	y++;
+}
+
+animate();
