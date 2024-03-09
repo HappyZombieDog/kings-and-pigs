478afa468440368bf5ca44841cbec0b04926a465
HappyZombieDog <magnus@graycastle.com>
2024-03-10 12:28:21 +1300

    KAP: Hitbox wip [video: 2:03:53]

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -10,7 +10,6 @@ class Player extends Sprite {
 			x: 0,
 			y: 0,
 		}
-
 	
 		this.sides = {
 			bottom:this.position.y + this.height,
@@ -21,12 +20,30 @@ class Player extends Sprite {
 	}
 
 	update() {
+		//this is the blue box
 		// c.fillStyle = 'rgba(0, 0, 255, 0.5)'
 		// c.fillRect(this.position.x, this.position.y, this.width, this.height)
+
 		this.position.x += this.velocity.x;
 				
 		this.checkForHorizontalCollisions();
 		this.applyGravity();
+
+		this.hitbox = {
+		position: {
+				x: this.position.x + 58,
+				y: this.position.y + 34,
+			},
+			width: 50,
+			height: 53,
+		}
+
+		c.fillRect(
+			this.hitbox.position.x, 
+			this.hitbox.position.y, 
+			this.hitbox.width, 
+			this.hitbox.height
+		)
 		this.checkForVerticalCollisions();
 	}
 
@@ -67,21 +84,28 @@ class Player extends Sprite {
 
 			// if a collision exists
 			if (
-				this.position.x <= collisionBlock.position.x + collisionBlock.width &&
-				this.position.x + this.width >= collisionBlock.position.x &&
-				this.position.y + this.height >= collisionBlock.position.y &&
-				this.position.y<= collisionBlock.position.y + collisionBlock.height
+				this.hitbox.position.x <= 
+					collisionBlock.position.x + collisionBlock.width &&
+				this.hitbox.position.x + this.hitbox.width >= 
+					collisionBlock.position.x &&
+				this.hitbox.position.y + this.hitbox.height >= 
+					collisionBlock.position.y &&
+				this.hitbox.position.y <= 
+					collisionBlock.position.y + collisionBlock.height
 			) {
 				if (this.velocity.y < 0) {
 					this.velocity.y = 0;
+					const offset = this.hitbox.position.y - this.position.y
 					this.position.y = 
-						collisionBlock.position.y + collisionBlock.height + 0.01;
+						collisionBlock.position.y + collisionBlock.height - offset+ 0.01;
 					break;
 				}
 
 				if (this.velocity.y > 0) {
 					this.velocity.y = 0;
-					this.position.y = collisionBlock.position.y - this.height - 0.01;
+					const offset = 
+						this.hitbox.position.y - this.position.y + this.hitbox.height
+					this.position.y = collisionBlock.position.y - offset - 0.01;
 					break;
 				}
 			}
