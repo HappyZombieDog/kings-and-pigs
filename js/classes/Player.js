7e7ecc37ffee7358b947804e45d2099d83e3aaf6
sgray <s.gray@circle-eight.com>
2024-03-12 19:08:52 +1300

    KAP: Hitbox implementation complete [video: 2:08:25]

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -25,26 +25,32 @@ class Player extends Sprite {
 		// c.fillRect(this.position.x, this.position.y, this.width, this.height)
 
 		this.position.x += this.velocity.x;
-				
+
+		this.updateHitbox();
+
 		this.checkForHorizontalCollisions();
 		this.applyGravity();
 
+		this.updateHitbox();
+
+		// c.fillRect(
+		// 	this.hitbox.position.x,
+		// 	this.hitbox.position.y,
+		// 	this.hitbox.width,
+		// 	this.hitbox.height
+		// )
+		this.checkForVerticalCollisions();
+	}
+
+	updateHitbox() {
 		this.hitbox = {
-		position: {
+			position: {
 				x: this.position.x + 58,
 				y: this.position.y + 34,
 			},
 			width: 50,
 			height: 53,
 		}
-
-		c.fillRect(
-			this.hitbox.position.x, 
-			this.hitbox.position.y, 
-			this.hitbox.width, 
-			this.hitbox.height
-		)
-		this.checkForVerticalCollisions();
 	}
 
 	checkForHorizontalCollisions() {
@@ -53,20 +59,27 @@ class Player extends Sprite {
 
 			// if a collision exists
 			if (
-				this.position.x <= collisionBlock.position.x + collisionBlock.width &&
-				this.position.x + this.width >= collisionBlock.position.x &&
-				this.position.y + this.height >= collisionBlock.position.y &&
-				this.position.y<= collisionBlock.position.y + collisionBlock.height
+				this.hitbox.position.x <=
+					collisionBlock.position.x + collisionBlock.width &&
+				this.hitbox.position.x + this.hitbox.width >=
+					collisionBlock.position.x &&
+				this.hitbox.position.y + this.hitbox.height >=
+					collisionBlock.position.y &&
+				this.hitbox.position.y <=
+					collisionBlock.position.y + collisionBlock.height
 			) {
 				// collision on x axis going to left
 				if (this.velocity.x < 0) {
+					const offset = this.hitbox.position.x - this.position.x;
 					this.position.x = 
-						collisionBlock.position.x + collisionBlock.width + 0.01;
+						collisionBlock.position.x + collisionBlock.width - offset + 0.01;
 					break;
 				}
 
 				if (this.velocity.x > 0) {
-					this.position.x = collisionBlock.position.x - this.width - 0.01;
+					const offset =
+						this.hitbox.position.x - this.position.x + this.hitbox.width;
+					this.position.x = collisionBlock.position.x - offset - 0.01;
 					break;
 				}
 			}
@@ -104,7 +117,7 @@ class Player extends Sprite {
 				if (this.velocity.y > 0) {
 					this.velocity.y = 0;
 					const offset = 
-						this.hitbox.position.y - this.position.y + this.hitbox.height
+						this.hitbox.position.y - this.position.y + this.hitbox.height;
 					this.position.y = collisionBlock.position.y - offset - 0.01;
 					break;
 				}
