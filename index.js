37e4d6d7e4d84f3b57d3cf259a13a05619e3105e
HappyZombieDog <magnus@graycastle.com>
2024-04-23 08:55:01 +1200

    KAP: Level 3 complete
    Video tutorial finished.

--- index.js
+++ index.js
@@ -4,19 +4,11 @@ const c = canvas.getContext('2d');
 canvas.width = 64 * 16; // 1024
 canvas.height = 64 * 9; // 576
 
-const parsedcollisions = collisionsLevel1.parse2D();
-const collisionBlocks = parsedcollisions.createObjectsFrom2D();
-
-const backgroundLevel1 = new Sprite({
-	position: {
-		x: 0,
-		y: 0,
-	},
-	imageSrc: './img/backgroundLevel1.png',
-});
-
+let parsedcollisions
+let collisionBlocks
+let background
+let doors
 const player = new Player({
-	collisionBlocks,
 	imageSrc: './img/king/idle.png',
 	frameRate: 11,
 	animations: {
@@ -50,26 +42,124 @@ const player = new Player({
 			loop: false,
 			imageSrc: './img/king/enterDoor.png',
 			onComplete: () => {
-				console.log('completed animaton')
+				console.log('completed animaton') 
+				gsap.to(overlay,{
+					opacity: 1,
+					onComplete: () => {
+						level++
+
+						if (level === 4) level = 1
+						levels[level].init();
+						player.switchSprite('idleRight')
+						player.preventInput = false
+						gsap.to(overlay, {
+							opacity: 0
+						})
+				   	},
+				})
 			},
 		},
-
 	},
 });
 
-const doors = [
- new Sprite({
- 	position: {
- 		x: 767,
- 		y: 270,
- 	},
- 	imageSrc: './img/doorOpen.png',
- 	frameRate:5,
- 	frameBuffer: 5,
- 	loop: false,
- 	autoplay: false,
- }),
-]
+let level = 1
+let levels = {
+	1: {
+		init: () => {
+			parsedcollisions = collisionsLevel1.parse2D();
+			collisionBlocks = parsedcollisions.createObjectsFrom2D();
+			player.collisionBlocks = collisionBlocks
+			if (player.currentAnimation) player.currentAnimation.isActive = false
+
+			background = new Sprite({
+				position: {
+					x: 0,
+					y: 0,
+				},
+				imageSrc: './img/backgroundLevel1.png',
+			});
+
+			doors = [
+				new Sprite({
+					position: {
+						x: 767,
+						y: 270,
+					},
+					imageSrc: './img/doorOpen.png',
+					frameRate:5,
+					frameBuffer: 5,
+					loop: false,
+					autoplay: false,
+				}),
+			]
+		},
+	},
+	2: {
+		init: () => {
+			parsedcollisions = collisionsLevel2.parse2D();
+			collisionBlocks = parsedcollisions.createObjectsFrom2D();
+			player.collisionBlocks = collisionBlocks
+			player.position.x = 96
+			player.position.y = 140
+
+			if (player.currentAnimation) player.currentAnimation.isActive = false
+
+			background = new Sprite({
+				position: {
+					x: 0,
+					y: 0,
+				},
+				imageSrc: './img/backgroundLevel2.png',
+			});
+
+			doors = [
+				new Sprite({
+					position: {
+						x: 772.0,
+						y: 336,
+					},
+					imageSrc: './img/doorOpen.png',
+					frameRate:5,
+					frameBuffer: 5,
+					loop: false,
+					autoplay: false,
+				}),
+			]
+		},
+	},
+	3: {
+		init: () => {
+			parsedcollisions = collisionsLevel3.parse2D();
+			collisionBlocks = parsedcollisions.createObjectsFrom2D();
+			player.collisionBlocks = collisionBlocks
+			player.position.x = 750
+			player.position.y = 230
+			if (player.currentAnimation) player.currentAnimation.isActive = false
+
+			background = new Sprite({
+				position: {
+					x: 0,
+					y: 0,
+				},
+				imageSrc: './img/backgroundLevel3.png',
+			});
+
+			doors = [
+				new Sprite({
+					position: {
+						x: 176,
+						y: 336,
+					},
+					imageSrc: './img/doorOpen.png',
+					frameRate:5,
+					frameBuffer: 5,
+					loop: false,
+					autoplay: false,
+				}),
+			]
+		},
+	},
+}
 
 const keys = {
 	ArrowUp: {
@@ -80,12 +170,17 @@ const keys = {
 	},
 	ArrowRight: {
 		pressed: false,
-	}
+	},
 }
+
+const overlay = {
+	opacity: 0
+}
+
 function animate() {
 	window.requestAnimationFrame(animate);
 	
-	backgroundLevel1.draw();
+	background.draw();
 	collisionBlocks.forEach(collisionBlock => {
 		collisionBlock.draw();
 	})
@@ -93,9 +188,18 @@ function animate() {
 	doors.forEach(door => {
 		door.draw();
 	})
+
 	player.handleInput(keys);
 	player.draw();
 	player.update();
+
+	c.save()
+	c.globalAlpha = overlay.opacity
+	c.fillStyle = 'black'
+	c.fillRect(0,0,canvas.width, canvas.height)
+	c.restore()
 }
+
+levels[level].init();
 
 animate();
